f25ff67f1d74ec4610bd3364028ad95c63576587
Shimpei Takamatsu <shimpeiws@gmail.com>
2020-09-12 23:06:30 +0900

    update

--- handler.py
+++ handler.py
@@ -1,11 +1,11 @@
 import subprocess
 import json
 import sys
-sys.path.append("/mnt/efs/lib")
+sys.path.append("/mnt/efs/my-dir/.venv/lib/python3.6/site-packages")
 import numpy as np
 
 def hello(event, context):
-    subprocess.run(["ls", "-l", "/mnt/efs"])
+    subprocess.run(["ls", "-l", "/mnt/efs/my-dir/.venv/lib/python3.6/site-packages"])
     subprocess.run(["pwd"])
 
     arr = np.asarray([1,2,3])
